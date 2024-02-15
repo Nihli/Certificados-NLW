f1f4c5fd9ecc4b0b5e17d1e10f50cf88961f343c
Lia Alflen <lia.mellon17@gmail.com>
2024-02-15 16:05:23 -0300

    create repository to our service

--- src/main/java/com/estudos/rocketseat/certificacaoNLW/modules/students/useCases/verifyIfHasCertificationUseCase.java
+++ src/main/java/com/estudos/rocketseat/certificacaoNLW/modules/students/useCases/verifyIfHasCertificationUseCase.java
@@ -1,13 +1,21 @@
 package com.estudos.rocketseat.certificacaoNLW.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudos.rocketseat.certificacaoNLW.modules.students.dto.VerifyHasCertificationDTO;
+import com.estudos.rocketseat.certificacaoNLW.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class verifyIfHasCertificationUseCase {
+
+    @Autowired
+    private CertificationStudentRepository repository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
-        if (dto.getEmail().equals("lia.alflen@gmail.com") && dto.getTechnology().equals("Java")) {
+        var result = this.repository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
